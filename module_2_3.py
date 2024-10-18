417b490e031a2dbc7c3e7fe63c600275024a9a0b
BiO-XX <bioxx85@gmail.com>
2024-10-18 20:15:40 +0300

    module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 k = 0
 n = len(my_list)
-while k != n:
+while 1 > 0:
     my_number = my_list[k]
     if my_number > 0:
         print(my_number)
