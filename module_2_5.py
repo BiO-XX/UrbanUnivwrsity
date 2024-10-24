f911507317bb6aa9890e1bfec3f2970c36700662
BiO-XX <bioxx85@gmail.com>
2024-10-24 22:33:11 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,14 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        list_1 = []
+        for j in range(m):
+            list_1.append(value)
+
+        matrix.append(list_1)
+
+    print(matrix)
+
+get_matrix(2,2,10)
+get_matrix(3,5,42)
+get_matrix(4,2,13)
