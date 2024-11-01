ac0bddab7d6a7ade9dba5b1d7da2ea4da3f5d8e6
BiO-XX <bioxx85@gmail.com>
2024-11-01 21:29:01 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -2,17 +2,17 @@ def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
-#1.Функция с параметрами по умолчанию:
+# 1.Функция с параметрами по умолчанию:
 print_params()
-print_params(b = 25)
-print_params(c = [1,2,3])
+print_params(b=25)
+print_params(c=[1, 2, 3])
 
-#2.Распаковка параметров:
+# 2.Распаковка параметров:
 values_list = [1, True, "Привет"]
 values_dict = {'a': 15000, 'b': 3500, 'c': 8800}
 print_params(*values_list)
 print_params(**values_dict)
 
-#3.Распаковка + отдельные параметры:
-values_list_2 = [1, True,]
+# 3.Распаковка + отдельные параметры:
+values_list_2 = [1, True, ]
 print_params(*values_list_2, 42)
