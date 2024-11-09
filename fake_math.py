90082301a0be870e4e8011bdc295ffefea54915c
BiO-XX <bioxx85@gmail.com>
2024-11-09 18:26:38 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def fake_divide(a, b):
+    if b == 0:
+        d = 'Ошибка'
+    else:
+        d = a / b
+    return d
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def true_divide(a, b):
+    if b == 0:
+        return inf
+    else:
+        d = a / b
+        return d
